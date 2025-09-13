e0d0612795777add8112789bf9d5e0d170a1284a
Kelvintbad <67743201+Kelvintbad@users.noreply.github.com>
2025-09-13 23:53:29 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client();
+const TARGET_CHANNEL_ID = '';
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+
+client.on('guildMemberAdd', async member => {
+  console.log(`Member ${member.user}`);
+  console.log(`Member ${member.user.tag} joined the guild ${member.guild.name}`);
+  const channel = client.channels.cache.get(TARGET_CHANNEL_ID);
+  if (!channel) return console.log(`Could not find channel with ID ${TARGET_CHANNEL_ID} in guild ${member.guild.name}`);
+
+  // Send the welcome message
+  await channel.send(`${member.user.tag} joined ${member.guild.name}
+  -------_-------`);
+})
+
+client.login('');
