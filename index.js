83d0676b753d2235b5097c586c242e95a0e9e368
Kelvintbad <67743201+Kelvintbad@users.noreply.github.com>
2025-09-14 04:29:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const TARGET_CHANNEL_ID = process.env.chnlID;
 const TOKEN = process.env.token;
 
 client.on('ready', async () => {
-  console.log(`${client.user.username} is ready!`);
+  await channel.send(`${client.user.username} is ready!`);
 })
 
 client.on('guildMemberAdd', async member => {
-  console.log(`Member ${member.user}`);
-  console.log(`Member ${member.user.tag} joined the guild ${member.guild.name}`);
+  // console.log(`Member ${member.user}`);
+  // console.log(`Member ${member.user.tag} joined the guild ${member.guild.name}`);
   const channel = client.channels.cache.get(TARGET_CHANNEL_ID);
-  if (!channel) return console.log(`Could not find channel with ID ${TARGET_CHANNEL_ID} in guild ${member.guild.name}`);
+  // if (!channel) return console.log(`Could not find channel with ID ${TARGET_CHANNEL_ID} in guild ${member.guild.name}`);
 
   // Send the welcome message
   await channel.send(`${member.user.tag} joined ${member.guild.name}
